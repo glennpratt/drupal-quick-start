a3b992fe8ed9f15e17cfe0e90ce2a9e1aa5f977d
Glenn Pratt <glennpratt@gmail.com>
2011-07-07 16:54:27 -0500

    Add various PHP modules Drupal needs built in.

--- roles/drupal.rb
+++ roles/drupal.rb
@@ -3,6 +3,10 @@ description "Drupal front end application server."
 run_list(
   "recipe[mysql::client]",
   "recipe[nfs]",
+  "recipe[php::module_curl]",
+  "recipe[php::module_gd]",
+  "recipe[php::module_mysql]",
+  "recipe[php::module_memcache]",
   "recipe[drush]",
   "recipe[drush::make]",
   "recipe[drupal::minimal]",
