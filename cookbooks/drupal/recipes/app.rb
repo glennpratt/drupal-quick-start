39a87a451308c60a95c5b35f5ebd31e6651290d1
Glenn Pratt <glennpratt@gmail.com>
2011-09-24 15:44:00 -0500

    Drush status can fail if the DB is deleted, change to drush sql-connect which should be more forgiving.

--- cookbooks/drupal/recipes/app.rb
+++ cookbooks/drupal/recipes/app.rb
@@ -161,7 +161,7 @@ deploy_revision app['id'] do
   })
   # TODO drush updatedb
   migrate true
-  migration_command 'drush status'
+  migration_command 'drush sql-connect'
   before_migrate do
     # TODO bash is supposed to be synchronous and execute isn't.
     # Is that right, what are the implications of async here?
