e54e93cdd2e9254ca061634fe890489857153e9b
Glenn Pratt <glennpratt@gmail.com>
2011-09-27 15:04:27 -0500

    Set InnoDB to flush to disc once per second... not ACID.

--- roles/drupal_database_master.rb
+++ roles/drupal_database_master.rb
@@ -3,3 +3,10 @@ description "Database master for the Drupal application."
 run_list(
   "recipe[database::master]"
 )
+default_attributes(
+  :mysql => {
+    :tunable => {
+      :innodb_flush_log_at_trx_commit => "2",
+    }
+  }
+)
